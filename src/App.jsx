0b06faad236414e01917354047830989ba8e7daa
Moch Farrel <mochfarrel2606@gmail.com>
2023-11-08 22:54:14 +0700

    Setup bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/home";
 import ProductPage from "./pages/products";
+import NavbarComponent from "./layouts/navbar";
+import FooterComponent from "./layouts/footer";
 
 function App() {
   return (
     <div>
+      <NavbarComponent />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductPage />} />
       </Routes>
+      <FooterComponent />
     </div>
   );
 }
