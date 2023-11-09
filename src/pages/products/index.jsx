a7af7e3a3d7a20256551a5138a14015be61a06e6
Moch Farrel <mochfarrel2606@gmail.com>
2023-11-09 18:40:51 +0700

    modification toggle grid

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,22 +1,33 @@
 import React, {useState} from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import products from "../../utils/data.json";
-import {StarFill} from "react-bootstrap-icons";
+import {StarFill, List, Grid} from "react-bootstrap-icons";
 import {Link} from "react-router-dom";
 import Form from "react-bootstrap/Form";
-
-function getProducts({page, limit}) {}
+import Stack from "react-bootstrap/Stack";
 
 function ProductPage() {
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(5);
+  const [toggleView, setToggleView] = useState(true);
+
+  const handleToggleView = () => {
+    setToggleView(!toggleView);
+  };
 
   return (
     <div className="w-100 min-vh-100 products-app">
       <Container>
-        <Row className="bg-primary justify-content-between">
-          <Col className="bg-warning">Appreance</Col>
+        <Row className="justify-content-between overflow-hidden mb-5">
+          <Col className="d-flex align-items-center ">
+            <p className="">Apprenace</p>
+            <Button onClick={handleToggleView} className="bg-white">
+              {toggleView ? (
+                <Grid size={10} color="#000000" />
+              ) : (
+                <List size={10} color="#000000" />
+              )}
+            </Button>
+          </Col>
           <Col>Sort by</Col>
           <Col>
             <Form.Select aria-label="Default select example">
@@ -27,7 +38,7 @@ function ProductPage() {
             </Form.Select>
           </Col>
         </Row>
-        <Row>
+        {/* <Row>
           {products.data.map((datas, i) => (
             <Col key={i}>
               <Card className="shadow-sm">
@@ -66,6 +77,62 @@ function ProductPage() {
                   </div>
                 </Card.Body>
               </Card>
+            </Col>
+          ))}
+        </Row> */}
+        <Row>
+          {products.data.map((datas, i) => (
+            <Col key={i} className={`mb-${toggleView ? 3 : 0}`}>
+              {toggleView ? (
+                <Card className="shadow-sm">
+                  <Card.Img variant="top" src={datas.primary_image.thumbnail} />
+                  <Card.Body>
+                    <Link to={`/products/${datas.product_id}`}>
+                      <Card.Text className="text-truncate">
+                        {datas.name}
+                      </Card.Text>
+                    </Link>
+                    <Card.Title>{datas.price.text_idr}</Card.Title>
+                    <div className="rating w-100 d-flex justify-content-between align-items-center">
+                      <div className="d-flex py-2 gap-1">
+                        <div className="d-flex gap-2 align-items-center">
+                          <StarFill size={14} color="#fed700" />
+                          <Card.Text>
+                            {datas.stats.averageRating
+                              ? datas.stats.averageRating
+                              : "0"}{" "}
+                            |
+                          </Card.Text>
+                        </div>
+                        <div>
+                          {datas.label_groups.map((labels, i) => {
+                            if (i >= 0 && i < 1) {
+                              return (
+                                <Card.Text key={i}>{labels.title}</Card.Text>
+                              );
+                            }
+                          })}
+                        </div>
+                      </div>
+                      <div>
+                        <Card.Text>Stock {datas.stock}</Card.Text>
+                      </div>
+                    </div>
+                  </Card.Body>
+                </Card>
+              ) : (
+                <Card className="w-100 bg-info flex-row flex-norap">
+                  <Card.Header as="h5">Featured</Card.Header>
+                  <Card.Body className="d-flex">
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                      With supporting text below as a natural lead-in to
+                      additional content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              )}
             </Col>
           ))}
         </Row>
