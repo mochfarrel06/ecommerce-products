b0c810e00a87f1a8bb075a32dde8532fe2dd84b3
Moch Farrel <mochfarrel2606@gmail.com>
2023-11-10 22:21:12 +0700

    memodifikasi landing page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className="home__page">
       <Container>
         <Row className="align-items-center justify-content-between">
-          <Col xs={2} md={6}>
+          <Col xs={12} md={6}>
             <div className="d-flex flex-column gap-4 mb-4">
               <h1 className="fs-1 fw-bolder lh-sm">
                 Which Focuses on the Distribution of Premium Gadget Accessories.
@@ -17,13 +17,17 @@ const HomePage = () => {
                 high-quality products to position our consumers at the highest
                 level possible.
               </p>
+              <Link to="/products">
+                <Button>Show Products</Button>
+              </Link>
             </div>
-            <Link to={"/products"}>
-              <Button>Show Products</Button>
-            </Link>
           </Col>
-          <Col className="d-flex justify-content-end">
-            <img src="./landingpage.png" alt="Gambar Landing Page" />
+          <Col xs={12} md={6} className="d-flex justify-content-end">
+            <img
+              src="./landingpage.png"
+              alt="Gambar Landing Page"
+              className="img-fluid"
+            />
           </Col>
         </Row>
       </Container>
